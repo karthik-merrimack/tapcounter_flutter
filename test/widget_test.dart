099c5ad620e73d90efdcf298a7bdd9dd47101551
karthik-merrimack <thirugnanask@merrimack.edu>
2025-05-02 12:43:23 -0500

    test widget updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tapcounter_flutter/main.dart';
@@ -13,18 +12,18 @@ import 'package:tapcounter_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TapCounterApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our counter starts at 0 taps.
+    expect(find.text("You've tapped 0 times"), findsOneWidget);
+    expect(find.text("You've tapped 1 time"), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'Tap Me!' button and trigger a frame.
+    await tester.tap(find.text('Tap Me!'));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("You've tapped 0 times"), findsNothing);
+    expect(find.text("You've tapped 1 time"), findsOneWidget);
   });
 }
